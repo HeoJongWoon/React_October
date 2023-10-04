979be5c58b1efc20f4b58855557012493e54c319
HeoJongWoon <whddns0902@gmail.com>
2023-10-04 14:33:13 +0900

    페이지 변경시마다 layout컴포넌트에 on클래스를 추가해서 페이지 전환모션 적용

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,9 +1,30 @@
 import styles from './Layout.module.scss';
 import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Layout({ title, children, styleName }) {
+	//컴포넌트 마운트시 변경할 state추가
+	const [IsOn, setIsOn] = useState(false);
+	//가상돔 요소를 document.querySelector를 쓰지않고 실시간으로 참조하고 싶을떄
+	//빈 참조객체를 useRef로 생성
+	const frame = useRef(null);
+
+	useEffect(() => {
+		console.log('page 변경');
+		//컴포넌트 마운트시 한번만 호출
+		//컴포넌트 마운트시 IsOn값을 true로 변경
+		//IsOn을 useRef가 아닌 state로 변경해야 되는 이유
+		//useRef값을 변경해도 리액트는 변경점을 인지 못해서 재렌더링이 안되기 때문에
+		setIsOn(true);
+	}, []);
+
 	return (
-		<section className={clsx(styles.layout, styleName)}>
+		//참조하고싶은 가상돔 요소에 ref로 연결
+		<section
+			ref={frame}
+			//IsOn State가 true 일때에만 on 클래스명 적용
+			className={clsx(styles.layout, styleName, IsOn ? styles.on : '')}
+		>
 			<figure></figure>
 
 			<div className={clsx(styles.content, styleName)}>
